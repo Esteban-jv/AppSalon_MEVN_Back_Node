a8ca813dc40440207c3f36cc84683e33259bffc1
= <=>
2024-04-30 13:03:34 -0500

    temporal fix

--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -7,16 +7,9 @@ router.route('/')
     .post(createService)
     .get(getServices)
 
-// router.post('/', createService)
-// router.get('/', getServices)
-
 router.route('/:id')
     .get(getServicesById)
     .put(updateService)
     .delete(deleteService)
-
-// router.get('/:id', getServicesById)
-// router.put('/:id', updateService)
-// router.delete('/:id', deleteService)
 
 export default router
